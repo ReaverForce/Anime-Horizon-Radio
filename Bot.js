ba096c48a284407c3e811cb4886110af7b12ff5c
ReaverForce <34511018+ReaverForce@users.noreply.github.com>
2017-12-13 14:52:22 +0100

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -18,4 +18,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(MzkwMTI4NjI3OTA5Mzk0NDMz.DRKyoQ.PMZJiykB6aG1YOiV6Got8wyOXOY);
+client.login(process.env.BOT_TOKEN);
